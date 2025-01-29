44b3df434c4fb4efca52613ec3791b1cd3895bdb
JongHyun070105 <bobi09361@gmail.com>
2025-01-29 19:49:02 +0900

    Java

--- src/main/java/ch01/Hello.java
+++ src/main/java/ch01/Hello.java
@@ -1,5 +1,12 @@
 package ch01; // 바이트 코드 파일이 위치할 패키지 선언
-
+/**
+ * @Park Jong Hyun
+ *
+ * **/
+/*
+* 2025년 1월 27일 ~
+*
+*/
 public class Hello { // Hello 클래스 선언
     public static void main(String[] args){ // main 메소드 선언
         System.out.println("ch01.Hello, Java"); // 콘솔에 출력하는 코드
